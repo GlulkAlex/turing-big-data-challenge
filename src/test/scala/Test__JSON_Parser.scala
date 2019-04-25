08911310f96789e39e493a9f2071c89cb7e243bb
GlulkAlex <alexanderglulkhovtsev@gmail.com>
2019-04-25 21:22:50 +0500

    Refactoring Test__JSON_Parser

--- src/test/scala/Test__JSON_Parser.scala
+++ src/test/scala/Test__JSON_Parser.scala
@@ -1,9 +1,12 @@
-package demo
+package big_data
+//package demo
 
 import com.fortysevendeg.lambdatest._
+import JSON_Parser.{ 
+    File_Props, get_Field_Name, get_Field_Value, file_Props_Json_Parser }
 
-
-class Test JSON_Parser extends LambdaTest {
+    
+class Test_JSON_Parser extends LambdaTest {
     // https://api.github.com/repos/pirate/crypto-trader/contents/symbols.py
 //# prettified ( actual respons is without spaces and lines ):
 val test_Input = """{
@@ -24,11 +27,7 @@ val test_Input = """{
     "html": "https://github.com/pirate/crypto-trader/blob/master/symbols.py"
   }
 }"""
-    // InputStream inputstream = new FileInputStream("c:\\data\\input-text.txt");
-    // ByteArrayInputStream(byte[] buf)
-    val ( f_1_N, b_It ) = get_Field_Name( 
-        //scala.io.Source.fromString( 
-        new scala.io.BufferedSource( 
+    val test_Input_BS = new scala.io.BufferedSource( 
             //?test_Input 
             //scala.io.Source.fromString( test_Input )
             // required: java.io.InputStream
@@ -36,7 +35,42 @@ val test_Input = """{
                 // required: Array[Byte]
                 test_Input.getBytes()
             )
-        )(scala.io.Codec.UTF8).buffered 
+        )(scala.io.Codec.UTF8)
+    val test_Input_SBS = scala.io.Source
+            .fromString( test_Input )
+    // prettified
+    // def scala.io.Source.fromFile(file: File, enc: String): BufferedSource
+    // def fromFile(name: String, enc: String): BufferedSource
+    val test_Input_1 = scala.io.Source.fromFile(
+        // required: java.io.File
+        //file 
+        name = "./src/test/resources/test_input_1.json", 
+        enc = "UTF8" 
+    )
+    // unprettified
+    val test_Input_2 = scala.io.Source.fromFile(
+        name = "./src/test/resources/test_input_2.json", 
+        enc = "UTF8" 
+    )
+    println( "unprettified test_Input:" )
+    println( test_Input.filter( (c: Char) => c != '\n' && c != ' ' ) )
+    println( "#" * 80 )
+    println( "test_Input_1:" )
+    println( test_Input_1 )
+    println( "#" * 80 )
+    // def scala.io.Source.createBufferedSource(
+    //  inputStream: InputStream, bufferSize: Int = DefaultBufSize, reset: () ⇒ Source = null, close: () ⇒ Unit = null)(implicit codec: Codec): BufferedSource
+    //  Reads data from inputStream with a buffered reader, 
+    //  using the encoding in implicit parameter codec.
+    
+    // InputStream inputstream = new FileInputStream("c:\\data\\input-text.txt");
+    // ByteArrayInputStream(byte[] buf)
+    val ( f_1_N, b_It ) = get_Field_Name( 
+        //>test_Input_BS
+        //>test_Input_SBS
+        //>test_Input_1
+        test_Input_2
+            .buffered
     )
     println( s"f_1_N: `${f_1_N}`" )
     val ( f_1_V, _ ) = get_Field_Value( b_It )
@@ -63,6 +97,6 @@ val test_Input = """{
   }
 }
 
-object Example extends App {
-  run("example", new Example)
+object Test_JSON_Parser extends App {
+  run( "Test-JSON-Parser", new Test_JSON_Parser )
 }
